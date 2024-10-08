f63c0ac48fc46cc4c6a88d22ffeb24e1e0add988
unknown <rasel mahmud>
2024-10-08 17:44:25 +0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using StudentManagementApp.View;
+using System.Reflection;
+using System;
+using System.Reflection;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+namespace StudentManagementApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+
+            UI user = new UI();
+
+          user.MainFile();
+
+        }
+
+    }
+}
